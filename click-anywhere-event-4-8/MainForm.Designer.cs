248b41d10cbc98b4f22df3fb84b010044c10731b
Tom Gregor <tgregor@ivsoftware.com>
2024-02-16 12:31:13 -0700

    Add project files.

---
+++ click-anywhere-event-4-8/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace click_anywhere_event_4_8
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonShowOther = new System.Windows.Forms.Button();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonShowOther
+            // 
+            this.buttonShowOther.Location = new System.Drawing.Point(12, 12);
+            this.buttonShowOther.Name = "buttonShowOther";
+            this.buttonShowOther.Size = new System.Drawing.Size(151, 44);
+            this.buttonShowOther.TabIndex = 0;
+            this.buttonShowOther.Text = "Show Other";
+            this.buttonShowOther.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.richTextBox.Location = new System.Drawing.Point(181, 10);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(287, 224);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 244);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.buttonShowOther);
+            this.Name = "MainForm";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonShowOther;
+        private System.Windows.Forms.RichTextBox richTextBox;
+    }
+}
+
---
+++ click-anywhere-event-4-8/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace click_anywhere_event_4_8
+{
+    public partial class MainForm : Form, IMessageFilter
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Application.AddMessageFilter(this);
+            Disposed += (sender, e) =>
+            {
+                Application.RemoveMessageFilter(this);
+                OtherForm.Dispose();
+            };
+            ClickAnywhere += (sender, e) =>
+            {
+                if (sender is Control parent)
+                {
+                    string message;
+                    switch (parent.GetType().Name)
+                    {
+                        case nameof(MainForm): richTextBox.SelectionColor = Color.Green; break;
+                        case nameof(OtherForm): richTextBox.SelectionColor = Color.Blue; break;
+                        default: richTextBox.SelectionColor = SystemColors.ControlText; break;
+                    }
+                    if (e.Control is Control child)
+                    {
+                        message = $"{parent.Name}.{child.Name}{Environment.NewLine}";
+                    }
+                    else message = $"{parent.Name}{Environment.NewLine}";
+                    richTextBox.AppendText(message);
+                }
+            };
+            buttonShowOther.Click += (sender, e) =>
+            {
+                // Pass 'this' to keep child form on top.
+                OtherForm.Show(this);
+            };
+        }
+        OtherForm OtherForm = new OtherForm();
+
+        private const int WM_LBUTTONDOWN = 0x0201;
+        public bool PreFilterMessage(ref Message m)
+        {
+            if(m.Msg == WM_LBUTTONDOWN && Control.FromHandle(m.HWnd) is Control control)
+            {
+                BeginInvoke((MethodInvoker)delegate
+                {
+                    ClickAnywhere?.Invoke(
+                        control.TopLevelControl,
+                        new ClickAnywhereEventArgs(control));
+                });
+            }
+            return false;
+        }
+        event EventHandler<ClickAnywhereEventArgs> ClickAnywhere;
+    }
+    delegate void ClickAnywhereEventHandler(Object sender, ClickAnywhereEventArgs e);
+    class ClickAnywhereEventArgs : EventArgs
+    {
+        public ClickAnywhereEventArgs(Control control) => Control = control;
+        public Control Control { get; }
+    }
+}
